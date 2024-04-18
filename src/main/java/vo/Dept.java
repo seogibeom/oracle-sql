72859ae7f4a02b6c2c215f1957381f18aee5ea1f
DESKTOP-G20BGD1\서기범 <rlqja1221@naver.com>
2024-04-18 10:16:12 +0900

    dept, emp 테이블 vo 생성

---
+++ src/main/java/vo/Dept.java
@@ -0,0 +1,12 @@
+package vo;
+
+
+// 	vo ( value Object 데이터 전송 객체를 구현할 때 사용)
+// 	Value Object는 주로 데이터를 캡슐화하고, 그 데이터에 대한 불변성을 유지하며, 관련된 메소드를 제공하여 객체를 쉽게 다루도록 도와주는 객체를 의미
+//	> DTO ( Date Transfer Object ) ,
+//domain ( 속성값의범위 )
+public class Dept {
+	public int deptno;
+	public String dname;
+	public String loc;
+}
---
+++ src/main/java/vo/Emp.java
@@ -0,0 +1,13 @@
+package vo;
+
+public class Emp {
+	public int emptno;
+	public String ename;
+	public String job;
+	public int mgr;
+	public String hiredate;
+	public double sal;
+	public double comm;
+	public int deptno;
+
+}
